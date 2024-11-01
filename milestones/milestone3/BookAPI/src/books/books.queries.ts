e1b152fe786bcea1b5d6dc5880baa06554e226bf
AntowanG-tech <antowang93@gmail.com>
2024-11-01 00:50:54 -0400

    adjused crud book queries to reflect database table properties

--- milestones/milestone3/BookAPI/src/books/books.queries.ts
+++ milestones/milestone3/BookAPI/src/books/books.queries.ts
@@ -8,12 +8,12 @@ export const bookQueries = {
 		`select bookID as bookID, title as title, author as author, publishedDate as publishedDate, ISBN as isbn, price as price, genreDesc as genre from library.books
 		where library.books.author like ?`, 
 	readBooksByBookID:
-		`select bookID as bookID, title as title, authorID as authorID, publishedDate as published, ISBN as isbn, price as price, genreDesc as genre from library.books
+		`select bookID as bookID, title as title, author as author, publishedDate as published, ISBN as isbn, price as price, genreDesc as genre from library.books
 	   where library.books.bookID = ?`,
 	createBook:
-		`insert into library.books(title, authorID, publishedDate, ISBN, price, genreDesc) values(?,?,?,?,?,?)`,
+		`insert into library.books(title, author, publishedDate, ISBN, price, genreDesc) values(?,?,?,?,?,?)`,
 	updateBook:
-		`update library.books set title=?, authorID=?, publishedDate=?, ISBN=?, price=?, genreDesc=? where bookID = ?`,
+		`update library.books set title=?, author=?, publishedDate=?, ISBN=?, price=?, genreDesc=? where bookID = ?`,
 	deleteBook:
 		`delete from library.books where bookID = ?`,
 }
